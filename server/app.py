eab2d179581b64734d4e8dee5126a45b68d87d3f
shekinah <nyamburamary1212@gmail.com>
2024-01-11 07:53:58 +0300

    Complete restful cr lab

--- server/app.py
+++ server/app.py
@@ -1,9 +1,6 @@
-#!/usr/bin/env python3
-
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
-
 from models import db, Plant
 
 app = Flask(__name__)
@@ -16,12 +13,39 @@ db.init_app(app)
 
 api = Api(app)
 
+
 class Plants(Resource):
-    pass
+    def get(self):
+        plant_dicts = [plant.to_dict() for plant in Plant.query.all()]
+        return make_response(plant_dicts, 200)
+
+    def post(self):
+        data = request.get_json()
+        new_plant = Plant(
+            name=data.get('name'),
+            image=data.get('image'),
+            price=data.get('price')
+        )
+        db.session.add(new_plant)
+        db.session.commit()
+        new_plant_dict = new_plant.to_dict()
+        return make_response(new_plant_dict, 201)
+
+
+api.add_resource(Plants, '/plants')
+
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, id):
+        plant = Plant.query.get(id)
+        if not plant:
+            return make_response({"error": "Plant not found"}, 404)
+
+        plant_dict = plant.to_dict()
+        return make_response(plant_dict, 200)
+
+
+api.add_resource(PlantByID, '/plants/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
 
@@ -7,3 +8,15 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    image = db.Column(db.String)
+    price = db.Column(db.Numeric(precision=10, scale=2))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    
+def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'image': self.image,
+            'price': str(self.price),  # Convert Numeric to string for JSON serialization
+        }
